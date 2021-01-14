81e33c25354e3a5d4afabbc91eee25f6d25c3bbd
Liam Cabral Teixeira <liamcabral88@gmail.com>
2021-01-14 03:36:28 -0300

    env config

---
+++ build/config/env/development.env.js
@@ -0,0 +1,13 @@
+"use strict";
+module.exports = {
+    env: 'development',
+    db: 'tsapi',
+    dialect: 'mysql',
+    username: 'root',
+    password: 'liam1514',
+    host: 'localhost',
+    serverPort: 3333,
+    mysqlPort: 3306,
+    dbURL: 'mysql://root:liam1514@localhost:3306/tsapi',
+    secret: 'S3cr3t',
+};
---
+++ build/config/env/env.js
@@ -0,0 +1,2 @@
+"use strict";
+module.exports = function () { return require('../env/development.env.ts'); };
